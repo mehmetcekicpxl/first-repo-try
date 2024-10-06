98bdac8b96861193f93aa02a1054948c717f3c0f
MehmetCekicPXL <mehmet.cekic@student.pxl.be>
2024-10-07 00:51:10 +0200

    wpf eklendi

---
+++ WpfAppIlkDeneme/WpfAppIlkDeneme/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfAppIlkDeneme
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
